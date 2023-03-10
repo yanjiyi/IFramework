e3f4ff5beed8ace5ae72a00ee07996672d8c4ff6
yanjiyi <630758569@qq.com>
2023-03-10 19:08:15 +0800

    IObject

--- IObject/test/IObject-Test.cpp
+++ IObject/test/IObject-Test.cpp
@@ -4,12 +4,15 @@ int main(int,char**)
 {
 	IObject a;
 	IObject b(&a);
-
+	IObject * d1 = new IObject(&b);
 	IObject * c = new IObject(&b);
 	IObject * d = new IObject(&b);
-
+	IObject * d2 = new IObject(d);
 	delete d;
 	
 	std::cout <<"Unrelease Pointer C : " << c << std::endl;
+	std::cout <<"Unrelease Pointer D1 : " << d1 << std::endl;
+	std::cout <<"Unrelease Pointer D2 - D : " << d2 << std::endl;
+
 	return 0;
 }
